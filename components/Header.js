6b69d8624a8ccd82327cb9d5a2c81555a8edbb51
chiuwa <chiuwa94@gmail.com>
2022-05-23 01:53:43 +0800

    add components

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css'
+
+const Header = () => {
+  return (
+    <div className={styles.header_top}>
+        <span className="flex items-center justify-center pt-2 text-white font-medium">Smart Home Products, Free Shipping Over $1,000 &#8594;</span>
+    </div>
+  )
+}
+
+export default Header
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {     
+       height: {
+      '128': '608px'
+    },
+    colors: {
+      'ling-greed': '#8dbd8d'
+    }
+   },
+  },
+  plugins: [],
+}

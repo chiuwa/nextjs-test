c5643fe0ec44bbc0cb6bc72acc9a210e4945631f
chiuwa <chiuwa94@gmail.com>
2022-05-24 18:50:09 +0800

    update parkage

--- components/ProductCarousel.js
+++ components/ProductCarousel.js
@@ -4,7 +4,11 @@ import styles from '../styles/Home.module.css'
 const ProductCarousel = () => {
     return (
         <div className={styles.container_f + " flex items-center justify-center lg:m-10 md:m-6 sm:m-3 mt-6 "}>
+            <div className='w-100'>
+                <span className='left-0'>All Categories</span>
+            </div>
             <Carousel max_width={2400}>
+
                 <img className="mr-5" src='https://via.placeholder.com/150' alt='imagem' title='imagem' />
                 <img className="mr-5" src='https://via.placeholder.com/150' alt='imagem' title='imagem' />
                 <img className="mr-5" src='https://via.placeholder.com/150' alt='imagem' title='imagem' />
